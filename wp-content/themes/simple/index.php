e12af68b77dbc1f3e3075133cda6661bc3279785
Thales <thalespizza@gmail.com>
2017-08-17 23:33:29 -0300

    aula 6 quase completa

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -13,11 +13,21 @@
 	<div class="container">
 		<?php if(have_posts()) : ?>
 			<?php while(have_posts()) : the_post() ?>
-				<h3><?php the_title() ?></h3>
+				<article class="post">
+				<h3>
+				<a href="<?php the_permalink() ?>">
+					<?php the_title() ?>	
+				</a>
+				</h3>
 				<div class="meta">
 					Criador por <?php the_author() ?> em <?php the_time('F j, Y g:ia') ?>
 				</div>
 				<?php the_content() ?>
+				<a class="button" href="<?php the_permalink() ?>">
+					Read More	
+				</a>
+				</article>
+				<hr>
 			<?php endwhile ?>
 		<?php else : ?>
 			<?php echo wpautop('Desculpe, nenhum post foi encontrado')?>
